4595fb5e1c70c12058159d92d105c31c3f593552
james <jguest777@gmail.com>
2020-10-09 14:05:16 -0400

    added axios to fetch API

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import './App.css';
 import Nav from './Components/Nav';
 import List from './Components/List';
 import Note from './Components/Note';
+import axios from 'axios';
 
 const App = () => {
   const [showNote, setShowNote] = useState(false);
 
   const toggleNote = () => setShowNote(!showNote);
 
+  const getNotes = async () => {
+    try {
+      const data = await axios.get('https://note-app-api.herokuapp.com/notes');
+      console.log(data.data);
+      return data;
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+
   return (
     <div className="App">
       <Nav toggleNote={toggleNote} showNote={showNote} />
-      {showNote ? <Note /> : <List />}
+      {showNote ? <Note /> : <List getNotes={getNotes} />}
     </div>
   );
 };
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const List = () => {
+const List = (props) => {
+  const { getNotes } = props;
+
+  useEffect(() => {
+    getNotes();
+  });
+
   return <div className="list-container">List Component</div>;
 };
 
