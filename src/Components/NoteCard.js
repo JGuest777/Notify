f5d47d426088b99f070aa4d1e663433e7470abbe
james <jguest777@gmail.com>
2020-10-09 15:17:17 -0400

    added icons to NoteCards and styled

---
+++ src/Components/NoteCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NoteCard = (props) => {
+  const { note } = props;
+
+  return (
+    <div className="note-card-container">
+      <div className="note-card-title">{note.title}</div>
+      <div className="note-card-content">{note.content}</div>
+      <span className="note-card-delete">
+        <i className="material-icons">close</i>
+      </span>
+      <span className="note-card-edit">
+        <i className="material-icons">mode_edit</i>
+      </span>
+    </div>
+  );
+};
+
+export default NoteCard;
