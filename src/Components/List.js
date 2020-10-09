e4f949afa909b316208339537f53fd0d950a0af5
james <jguest777@gmail.com>
2020-10-09 15:03:49 -0400

    added NoteCard component as child of List

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
+import NoteCard from './NoteCard';
 
 const List = (props) => {
-  const { getNotes } = props;
+  const { getNotes, notes } = props;
 
   useEffect(() => {
     getNotes();
   });
 
-  return <div className="list-container">List Component</div>;
+  const cards = notes.map((note, index) => {
+    return <NoteCard key={index} index={index} note={note} />;
+  });
+
+  return <div className="list-container">{cards}</div>;
 };
 
 export default List;
