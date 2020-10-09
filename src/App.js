6e7731f0911bfbf2336fc80d2946344c9b8f6c94
james <jguest777@gmail.com>
2020-10-09 11:49:12 -0400

    added List and Note components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Nav from './Components/Nav';
+import List from './Components/List';
+import Note from './Components/Note';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <List />
+      <Note />
     </div>
   );
 }
