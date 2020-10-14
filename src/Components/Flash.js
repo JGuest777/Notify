a0bcec6cf9363d3b1fcd1212637b17c8fd5dc7d8
james <jguest777@gmail.com>
2020-10-14 14:17:28 -0400

    fixed flash error size for mobile, adjusted noteCardForm

--- src/Components/Flash.js
+++ src/Components/Flash.js
@@ -2,10 +2,9 @@ import React, { useEffect } from 'react';
 
 const Flash = ({ error, resetError }) => {
   useEffect(() => {
-    console.log('setTimeout');
     setTimeout(() => {
       resetError();
-    }, 2000);
+    }, 3000);
   });
 
   return <div className="flash-container">{error}</div>;
